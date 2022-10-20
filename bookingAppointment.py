f206c450f992a1acdcb052cdddb18d35db8d7526
kmalott <kmalott2@gmail.com>
2022-10-20 12:56:53 -0700

    Combined bookingAppointment to one method

--- bookingAppointment.py
+++ bookingAppointment.py
@@ -1,27 +1,14 @@
-def start():
+def run():
+    user_input = ""
     print("Hi! Welcome to the Appointment Booking Feature. To return to the home at any time type ""exit"".")
-    return
-
-def exit():
-    print("Exiting Appointment Booking")
-    return
-        
-def appointment():
     print("To book an academic advising appointment please follow the link provided below.")
     print("https://students.ok.ubc.ca/academic-success/advising-options/academic-advising/contact/")
-    return
+    while user_input != "exit":
+        user_input = str(input())
+        if user_input == "exit":
+            print("Exiting Appointment Booking")
+            return
+        else: 
+            print("Your input is not recognised. To exit this feature please type exit")
 
-def error():
-    print("Your input is not recognised. To exit this feature please type exit")
-    return
-
-user_input = ""
-
-start()
-appointment()
-while user_input != "exit":
-    user_input = str(input())
-    if user_input == "exit":
-        exit()
-    else: 
-        error()
+run()
