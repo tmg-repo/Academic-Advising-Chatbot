15ad08ed975721b24d10166a5af03dab8737aad6
kmalott <kmalott2@gmail.com>
2022-10-31 12:26:05 -0700

    added sentiment analysis to main

--- code/main.py
+++ code/main.py
@@ -1,5 +1,12 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+
 def main():
+    sent = SentimentIntensityAnalyzer()
     ans = input("What can I help you with today? :)\n")
+    sent_dict = sent.polarity_scores(ans)
+    max_value = max(sent_dict, key=sent_dict.get)
     
     help = "help"
     prereqs = "prerequisites"
@@ -14,7 +21,15 @@ def main():
     true = 0
     
     while true == 0:
-    
+        if max_value == "neg":
+            ans = input("I'm sorry this conversation wasn't helpful. Would you like to continue?\n")
+            if "no" in ans:
+                print("test")
+                return
+            else:
+                main()
+                break
+
         if courses in ans:
             from coursesAvail import coursesAvail
             coursesAvail()
@@ -35,9 +50,21 @@ def main():
             get_help()
             
         ans = input("What else can I help you with?\n")
+        sent_dict = sent.polarity_scores(ans)
+        max_value = max(sent_dict, key=sent_dict.get)
         if nothing in ans or nothing2 in ans:
-            print("Thank you for talking with me, I hope this was helpful!")
+            ans = input("How did you feel about our conversation?")
+            sent_dict = sent.polarity_scores(ans)
+            max_value = max(sent_dict, key=sent_dict.get)
+
+            if max_value == "neg":
+                print("I'm sorry I wasn't very useful. Thank you for the feedback!")
+            elif max_value == "neu" or max_value == "compound":
+                print("Thanks for the feedback!")
+            elif max_value == "pos":
+                print("I'm glad you had a good conversation with me!")
             true = 1
+            
 
     
 
